776ab5df7838de7744f701f1f38188ad29decb45
dinisahfitri02 <dinisahfitriiiii@gmail.com>
2023-09-22 09:46:41 +0700

    kuis 1

---
+++ main.pas
@@ -0,0 +1,15 @@
+uses wincrt;
+var
+nama,kom,alamat:string;
+nim:integer;
+begin
+write('masukkan nama anda: ');
+readln(nama);
+write('masukkan nim anda: ');
+readln(nim);
+write('masukkan kom anda: ');
+readln(kom);
+write('masukkan alamat anda');
+readln(alamat);
+writeln('hallo nama saya',nama,'nim saya',nim,'saya di kom',kom,'alamat saya di',alamat,'.salam kenal semuanya!')
+end.
